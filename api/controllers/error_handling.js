b993b694ea47e08af0eb0056364842fbc78bd9ee
Adam <linfordaj@gmail.com>
2022-10-11 11:20:56 +0100

    add task 4  with error handling, now in seperate file

---
+++ api/controllers/error_handling.js
@@ -0,0 +1,25 @@
+
+//PSQL errors
+exports.PSQLErrorHandling = (err, req, res, next) => {
+    if(err.code === '22P02'){
+        res.status(400).send({msg: 'Invalid input type'})
+    }else{
+        next(err);
+    }
+};
+
+//custom errors
+exports.customErrorHandling = (err, req, res, next) => {
+    if(err.status){
+        res.status(err.status).send({msg: err.msg});
+    }else{
+        next(err);
+    }
+};
+
+//500
+
+exports.internalErrorHandling = (err, req, res, next) => {
+    res.status(500).send({msg: 'Internal server error'});
+};
+
