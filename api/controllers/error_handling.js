258396eae52e7c2f1c8ee0b82fa54ca4e9e8798d
Adam <linfordaj@gmail.com>
2022-10-13 14:59:30 +0100

    10-POST-api-reviews-review_id-comment and tests

--- api/controllers/error_handling.js
+++ api/controllers/error_handling.js
@@ -1,7 +1,7 @@
 
 //PSQL errors
 exports.PSQLErrorHandling = (err, req, res, next) => {
-    if(err.code === '22P02'){
+    if(err.code === '22P02' || err.code === '23503'){
         res.status(400).send({msg: 'Invalid input type'})
     }else{
         next(err);
@@ -18,8 +18,8 @@ exports.customErrorHandling = (err, req, res, next) => {
 };
 
 //500
-
 exports.internalErrorHandling = (err, req, res, next) => {
+    console.log(err);
     res.status(500).send({msg: 'Internal server error'});
 };
 
